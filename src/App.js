61d2a01328098fb8e0b7f1c927b4ce122a9b05ef
Sushmita Kumari <63455991+sushmita24680@users.noreply.github.com>
2022-10-12 03:33:57 +0530

    added props concept

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
+/** @format */
+
 // import logo from './logo.svg';
 // import './App.css';
-import ExpensesItem from "./components/ExpensesItem";
+import ExpensesItem from './components/ExpensesItem';
 function App() {
+  const expense = [
+    {
+      id: 'e1',
+      date: new Date(2021, 2, 28),
+      title: ' car Insurance',
+      amount: 297.64,
+    },
+    {
+      id: 'e2',
+      date: new Date(2021, 2, 11),
+      title: ' Grossary',
+      amount: 1237.9,
+    },
+    {
+      id: 'e3',
+      date: new Date(2021, 2, 7),
+      title: ' Semester fee',
+      amount: 59700.0,
+    },
+    {
+      id: 'e4',
+      date: new Date(2021, 2, 2),
+      title: ' Toilet Paper',
+      amount: 507.64,
+    },
+    {
+      id: 'e5',
+      date: new Date(2021, 2, 18),
+      title: ' New Desktop',
+      amount: 12905.34,
+    },
+  ];
   return (
-    <div className="App">
-<h1>start learning</h1>
-<ExpensesItem />
+    <div className='App'>
+      <h1>start learning</h1>
+      <ExpensesItem
+        date={expense[0].date}
+        title={expense[0].title}
+        amount={expense[0].amount}
+      ></ExpensesItem>
+      <ExpensesItem
+        date={expense[1].date}
+        title={expense[1].title}
+        amount={expense[1].amount}
+      ></ExpensesItem>
+      <ExpensesItem
+        date={expense[2].date}
+        title={expense[2].title}
+        amount={expense[2].amount}
+      ></ExpensesItem>
+      <ExpensesItem
+        date={expense[3].date}
+        title={expense[3].title}
+        amount={expense[3].amount}
+      ></ExpensesItem>
     </div>
   );
 }
--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,20 +1,20 @@
 /** @format */
 import './ExpensesItem.css';
 
-function ExpensesItem() {
-const expenseDate = new Date(2021,2,28);
-const expenseTitle = 'Car Insurance';
-const  expensePrice = 294.67;
+function ExpensesItem(props) {
+    const expenseDate = props.date;
+    const expenseTitle = props.title;
+    const expensePrice = props.amount;
 
     return (
-    <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h1>{expenseTitle}</h1>
-        <div className='expense-item__price'>${expensePrice}</div>
-      </div>
-    </div>
-  );
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h1>{expenseTitle}</h1>
+                <div className='expense-item__price'>${expensePrice}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpensesItem;
